898dd2eec97705f1d6e611c812eae54fb38933d6
Raghda  Muayad <therose000777@gmail.com>
2021-05-04 14:43:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ console.log(multiply)
 //function getremainder() {
 let num3=16;
 let num4=3;
-let mod =num3% num4;
+let mod =num3 % num4;
 console.log(mod)
 //{
 //  return getremainder.remainder ;

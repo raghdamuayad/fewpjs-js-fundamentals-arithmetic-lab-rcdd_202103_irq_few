0021f07c012c65071c0b625bd4b4e17421d49ead
Raghda  Muayad <therose000777@gmail.com>
2021-05-04 13:41:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+function multiply() {
+let num1= 1;
+let num2 = 62;
+let multiply = num1 * num2
+console.log(multiply)
+
+}
+function getremainder() {
+let y=16;
+let x=3;
+let remainder = y % x;
+{
+  return getremainder.remainder ;
+}
+}
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+}

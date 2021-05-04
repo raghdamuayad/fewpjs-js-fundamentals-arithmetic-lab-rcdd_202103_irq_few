ada89fe536600cb2b5453e9745efc41959e93d33
Raghda  Muayad <therose000777@gmail.com>
2021-05-04 15:00:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+let num1= 1;
+let num2 = 62;
+let multiply = num1 * num2
+console.log(multiply)
+
+
+let num3 =16;
+let num4 =6;
+let mod =num3 % num4;
+console.log(mod)
+
+      let max=Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+
+
+ let random = Math.floor(Math.random() * 10) + 1;

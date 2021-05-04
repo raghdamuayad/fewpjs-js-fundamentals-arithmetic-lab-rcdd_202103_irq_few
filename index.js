0ac8072a02d99c9aafe3ecfe9a4d0e2cb1c4954e
Raghda  Muayad <therose000777@gmail.com>
2021-05-04 14:39:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 // Write your code here
-function multiply() {
+//function multiply() {
 let num1= 1;
 let num2 = 62;
 let multiply = num1 * num2
 console.log(multiply)
 
-}
+//}
 function getremainder() {
 let y=16;
 let x=3;

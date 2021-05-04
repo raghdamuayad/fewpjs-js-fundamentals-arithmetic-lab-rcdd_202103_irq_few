ac9683222e8ac2aca6734aeaa27de1d8c52620a1
Raghda  Muayad <therose000777@gmail.com>
2021-05-04 14:58:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,3 +23,6 @@ console.log(mod)
 
       let max=Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
 //}
+
+ let random = Math.floor(Math.random() * 10) + 1;
+ //let random = Math.random ()
